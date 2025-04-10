c6f756251dcb23a28d36fca48f755c3772360578
My Tran <thaomytrn127@gmail.com>
2025-04-10 13:03:33 -0400

    update create account

--- js/client.js
+++ js/client.js
@@ -1,99 +1,72 @@
+/*
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("C:/Users/valer/Documents/ISTM 4210 Capstone/resourcelink-80257-firebase-adminsdk-fbsvc-c54a358f87.json");
+
+//Initialize Firebase Admin SDK
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://resourcelink-80257-default-rtdb.firebaseio.com"
+});
+
+const db = admin.database();
+const clientDB = db.ref('client');
+  */
 //Firebase Frontend Configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDNTrGx7vfJDB6mQL7XBSPo2DqCSACVjDM",
-    authDomain: "resourcelink-80257.firebaseapp.com",
-    databaseURL: "https://resourcelink-80257-default-rtdb.firebaseio.com",
-    projectId: "resourcelink-80257",
-    storageBucket: "resourcelink-80257.firebasestorage.app",
-    messagingSenderId: "249943715055",
-    appId: "1:249943715055:web:03022ed87d6a42acdcbf1a"
+  apiKey: "AIzaSyDNTrGx7vfJDB6mQL7XBSPo2DqCSACVjDM",
+  authDomain: "resourcelink-80257.firebaseapp.com",
+  databaseURL: "https://resourcelink-80257-default-rtdb.firebaseio.com",
+  projectId: "resourcelink-80257",
+  storageBucket: "resourcelink-80257.firebasestorage.app",
+  messagingSenderId: "249943715055",
+  appId: "1:249943715055:web:03022ed87d6a42acdcbf1a"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+function submitData() {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  var userName = document.getElementById('username').value;
+  var phone = document.getElementById('phone').value;
+  var consent = document.getElementById('consent').checked ? 'Yes' : 'No';
+  var ethnicity = document.querySelector('input[name="ethnicity"]:checked').value;
+  var gender = document.querySelector('input[name="gender"]:checked').value;
+  var age = document.querySelector('input[name="age"]:checked').value;
+  var education = document.querySelector('input[name="education"]:checked').value;
+  var employment = document.querySelector('input[name="employment"]:checked').value;
+
+  const data = {
+    email: email,
+    password, password,
+    userName: userName,
+    phoneNumber: phone,
+    dataConsent: consent,
+    ethnicity: ethnicity,
+    gender: gender,
+    age: age,
+    education: education,
+    employmentStatus: employment
   };
-  
-//firebase.initializeApp(firebaseConfig);
-/*  
 
-function submitDataProvider() {
-    var organization = document.getElementById('organization').value;
-    var phone = document.getElementById('phone').value;
-    var bio = document.getElementById('bio').value;
-    var address = document.getElementById('address').value;
-    var zip = document.getElementById('zip').value;
-    var city = document.getElementById('city').value;
-    var state = document.getElementById('state').value;
-    var country = document.getElementById('country').value;
-    var resources = [];
-    document.querySelectorAll('input[name="resource"]:checked').forEach((checkbox) => {
-        resources.push(checkbox.value);
-    });
-    var hours = document.getElementById('hours').value;
-
-    firebase.database().ref('providers').push({
-        organization: organization,
-        phone: phone,
-        bio: bio,
-        address: address,
-        zip: zip,
-        city: city,
-        state: state,
-        country: country,
-        resources: resources,
-        hours: hours
-    }, function(error) {
-        if (error) {
-          alert('Data could not be saved.' + error);
-        } else {
-          alert('Data saved successfully.');
-        }
-      }
-
-);
-}
-*/
-
-function submitDataProvider() {
-    const organization = document.getElementById('organization').value;
-    const phone = document.getElementById('phone').value;
-    const bio = document.getElementById('bio').value;
-    const address = document.getElementById('address').value;
-    const zip = document.getElementById('zip').value;
-    const city = document.getElementById('city').value;
-    const state = document.getElementById('state').value;
-    const country = document.getElementById('country').value;
-    const resources = [];
-    document.querySelectorAll('input[name="resource"]:checked').forEach((checkbox) => {
-        resources.push(checkbox.value);
-    });
-    const hours = document.getElementById('hours').value;
-
-    const data = {
-        organization,
-        phone,
-        bio,
-        address,
-        zip,
-        city,
-        state,
-        country,
-        resources,
-        hours
-    };
-
-    fetch('http://localhost:3000/submit-providers', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then((response) => {
-        if (!response.ok) throw new Error("Network response was not ok.");
-        return response.text();
-    })
-    .then((msg) => {
-        alert(msg);
-        window.location.href = 'payment.html'; // Redirect to the payment page after successful submission
-    })
-    .catch((error) => {
-        alert('Failed to save provider information. ' + error.message);
-    });
+  fetch('http://localhost:3000/submit-data', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then((response) => {
+    if (!response.ok) throw new Error("Network response was not ok.");
+    return response.text();
+  })
+  .then((msg) => {
+    alert(msg);
+  })
+  .catch((error) => {
+    alert('Failed to save client data. ' + error.message);
+  });
 }
